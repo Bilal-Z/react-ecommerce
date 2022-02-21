6144fe44838b4dfc490f00da7572c42cae584844
Bilal <bilalzubairi@hotmail.com>
2022-02-21 10:50:32 +0500

    initial-commit

---
+++ src/homepage.component.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+	return (
+		<div className="homepage">
+			<div className="directory-menu">
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">PROCESSORS</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">MOTHERBOARDS</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">MEMORY MODULES</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">POWER SUPPLIES</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">SOLID STATE DRIVES</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">HARD DISK DRIVES</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">GRAPHICS CARDS</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">CPU COOLERS</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">CASE FANS</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title">COMPUTER CASES</h1>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
